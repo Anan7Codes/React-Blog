0b119889453ac38f970561f44eef78c7fea7f64d
Anandhu <anand7u@pop-os.localdomain>
2020-05-07 21:42:04 +0400

    Intial Heroku DeployFinalKeerth

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.json({"Hello": "I Am Happy To Deploy My Application"})
+    res.json({"Ugly": "Keerthu duh why is there a doubt"})
 });
 
 app.get('/api/user/auth', auth, (req, res) => {
